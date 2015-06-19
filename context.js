9fd4a4aa8d1aeaf61c2a3cb674f18c55fd4a4be5
Jason Tsukamoto <jasontsukamoto@gmail.com>
2015-06-19 09:40:05 -1000

    all tests pass

--- context.js
+++ context.js
@@ -1,20 +1,18 @@
 var Robot = function (name) {
   this.name = name;
-
-}
+};
 
 function add ( op1, op2 ) {
-  this.name = this.name || "Humans";
-  return this.name + " can count to " + (op1 + op2);
+  this.name = this.name || 'Humans';
+  return this.name + ' can count to ' + (op1 + op2);
 }
 
-var voltron = new Robot("Voltron");
-var bender = new Robot("Bender Rodriguez");
-var optimus = new Robot("Optimus Prime");
-var megaman = new Robot("Mega Man");
-var bmo = new Robot("B-Mo");
-var wall_e = new Robot("Wall-E");
-
+var voltron = new Robot('Voltron');
+var bender = new Robot('Bender Rodriguez');
+var optimus = new Robot('Optimus Prime');
+var megaman = new Robot('Mega Man');
+var bmo = new Robot('B-Mo');
+var wall_e = new Robot('Wall-E');
 
 // #1
 console.log(add(0, 1));
@@ -23,8 +21,8 @@ console.log(add(0, 1));
 console.log(add.call(voltron, 2, 3));
 
 // #3
-console.log(add.apply(optimus,[20,30]));
+console.log(add.apply(optimus, [20, 30]));
 
-// #4
+// #4ga
 var calculate = add.bind(bender);
 console.log(calculate('drinking', 'beer'));
